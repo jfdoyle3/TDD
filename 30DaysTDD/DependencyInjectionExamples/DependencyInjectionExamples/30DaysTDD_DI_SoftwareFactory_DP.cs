15fbdc2558fb04941fd7ce2be139fe2cda48dbb1
Jim Doyle <indellibleink@gmail.com>
2019-09-19 00:52:34 -0400

    30DaysTDD : Dependency Injection examples and Ninject Framework

---
+++ 30DaysTDD/DependencyInjectionExamples/DependencyInjectionExamples/30DaysTDD_DI_SoftwareFactory_DP.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticallyBoundLibrary
+{
+    public class BusinessServiceFactory
+    {
+        public static BusinessService GetNewBusinessService()
+        {
+            var dataStoreProvider = GetNewDataStoreProvider();
+            var loggingProvider = GetNewLoggingProvider();
+            var webServiceProvider = GetNewWebServiceProvider();
+
+            return new BusinessService(dataStoreProvider, loggingProvider, webServiceProvider);
+        }
+
+        private static IWebServiceProvider GetNewWebServiceProvider()
+        {
+            return new WebServiceProvider();
+        }
+
+        private static ILoggingProvider GetNewLoggingProvider()
+        {
+            return new LoggingProvider();
+        }
+
+        private static IDataStoreProvider GetNewDataStoreProvider()
+        {
+            return new DataStoreProvider();
+        }
+    }
+
+    public class BusinessService
+    {
+        private IDataStoreProvider _dataStoreProvider;
+        private ILoggingProvider _loggingProvider;
+        private IWebServiceProvider _webServiceProvider;
+
+        public BusinessService(IDataStoreProvider dataStoreProvider,
+            ILoggingProvider loggingProvider, IWebServiceProvider webServiceProvider)
+        {
+            _dataStoreProvider = dataStoreProvider;
+            _loggingProvider = loggingProvider;
+            _webServiceProvider = webServiceProvider;
+        }
+    }
+
+    public class DataStoreProvider : IDataStoreProvider
+    {
+    }
+
+    public interface IDataStoreProvider
+    {
+    }
+
+    public class LoggingProvider : ILoggingProvider
+    {
+    }
+
+    public interface ILoggingProvider
+    {
+    }
+
+    public class WebServiceProvider : IWebServiceProvider
+    {
+    }
+
+    public interface IWebServiceProvider
+    {
+    }
+}
---
+++ 30DaysTDD/DependencyInjectionExamples/DependencyInjectionExamples/30DaysTDD_NinjectFramework.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ninject.Modules;
+
+namespace DependencyInjectionExamples
+{
+    class _30DsysTDD_Ninject
+    {
+        public class DomainModule : NinjectModule
+        {
+            public override void Load()
+            {
+                Bind<IDataStoreProvider>().To<DataStoreProvider>();
+                Bind<ILoggingProvider>().To<LoggingProvider>();
+                Bind<IWebServiceProvider>().To<WebServiceProvider>();
+            }
+        }
+         public class BusinessServiceConsumer
+        {
+            private BusinessService _businessService;
+
+            public BusinessServiceConsumer()
+            {
+                var kernel = new StandardKernel(new DomainModule());
+
+                _businessService = kernel.Get<BusinessService>();
+
+
+            }
+
+        }
+        public class DataStoreProvider : IDataStoreProvider
+        {
+
+          private ILoggingProvider _loggingProvider;
+
+ 
+
+          public DataStoreProvider(ILoggingProvider loggingProvider)
+          {
+
+              this._loggingProvider = loggingProvider;
+
+          }
+
+        }        
+    }
+}
