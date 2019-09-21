1fe2153a5c8f12e1a6b7f7e90e7c1e9ab5adbd90
Jim Doyle <indellibleink@gmail.com>
2019-09-21 11:24:13 -0400

    TDD Day 11: Mocking

---
+++ 30DaysTDD/Mocking/Mocking/OrderService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    class OrderService
+    {
+        private ICustomerService _customerService;
+        private ILoggingService _loggingService;
+        private IOrderDataService _orderDataService;
+
+        public OrderService(IOrderDataService orderDataService, ICustomerService customerService, ILoggingService loggingService)
+        {
+            _orderDataService = orderDataService;
+            _customerService = customerService;
+            _loggingService = loggingService;
+        }
+
+        public Guid PlaceOrder(Guid customerId, IShoppingCart shoppingCart)
+        {
+            var order = new Order();
+
+            // Business logic that validates order and creates Order object
+
+            var orderId = Save(order);
+
+            _customerService.AddOrderToCustomer(customerId, orderId);
+            _loggingService.LogNewOrder(orderId);
+
+            return orderId;
+        }
+
+        private Guid Save(Order order)
+        {
+            return _orderDataService.Save(order);
+        }
+    }
+
+    public interface IOrderDataService
+    {
+        Guid Save(Order order);
+    }
+
+    public interface ICustomerService
+    {
+        bool AddOrderToCustomer(Guid customerId, Guid orderId);
+    }
+
+    public interface ILoggingService
+    {
+        bool LogNewOrder(Guid orderId);
+    }
+
+    public interface IShoppingCart
+    {
+
+    }
+
+    public class Order
+    {
+
+    }
+}
--- 30DaysTDD/ThirtyDaysOfTDD/ThirtyDaysOfTDD/StringUtils.cs
+++ 30DaysTDD/ThirtyDaysOfTDD/ThirtyDaysOfTDD/StringUtils.cs
@@ -26,6 +26,7 @@ namespace ThirtyDaysOfTDD.UnitTests
                  numberOfOccurences++;
               }
            }
+
            return numberOfOccurences;   
         }
     }
