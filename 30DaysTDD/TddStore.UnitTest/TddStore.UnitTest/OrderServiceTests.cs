be5c9803d1d13159e7edfefcc2f27a3d5c7f7dce
Jim Doyle <indellibleink@gmail.com>
2019-09-22 06:51:40 -0400

    Completed Day 12 30d TDD

---
+++ 30DaysTDD/TddStore.UnitTest/TddStore.UnitTest/OrderServiceTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using TddStore.Core;
+using Telerik.JustMock;
+
+namespace TddStore.UnitTests
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void WhenUserPlacesACorrectOrderThenAnOrderNumberShouldBeReturned()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.Items.Add(new ShoppingCartItem { ItemId = Guid.NewGuid(), Quantity = 1 });
+            var customerId = Guid.NewGuid();
+            var expectedOrderId = Guid.NewGuid();
+
+            var orderDataService = Mock.Create<IOrderDataService>();
+            Mock.Arrange(() => orderDataService.Save(Arg.IsAny<Order>())).Returns(expectedOrderId);
+            var orderService = new OrderService(orderDataService);
+
+            //Act
+            var result = orderService.PlaceOrder(customerId, shoppingCart);
+
+            //Assert
+            Assert.AreEqual(expectedOrderId, result);
+        }
+    }
+}
